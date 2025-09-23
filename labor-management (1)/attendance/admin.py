fee72777b466ac57c1c24206c2182da07244143b
ummara-artistic <ummara.khan@artisticmilliners.com>
2025-09-23 14:16:58 +0500

    Delete labor-management (1) directory

--- labor-management (1)/attendance/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Attendance
-
-@admin.register(Attendance)
-class AttendanceAdmin(admin.ModelAdmin):
-    list_display = ['employee', 'date', 'status', 'in_time', 'out_time', 'overtime_hours']
-    list_filter = ['status', 'date', 'employee__employee_type']
-    search_fields = ['employee__name', 'employee__last_name']
-    date_hierarchy = 'date'
