c00e2a8027d6f9963567c5dea79622a57a71e4cd
ummara-artistic <ummara.khan@artisticmilliners.com>
2025-09-25 17:23:20 +0500

    Update settings.py

--- labor_management/settings.py
+++ labor_management/settings.py
@@ -36,9 +36,10 @@ WSGI_APPLICATION = 'labor_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
+
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Karachi'
